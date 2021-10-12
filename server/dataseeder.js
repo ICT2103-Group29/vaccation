5458d4a366855d4d84117852c70918f87cfd8547
HanYi <nghanyi1997@gmail.com>
2021-10-13 01:20:10 +0800

    #5 create function to insert country vaccinations data

--- server/dataseeder.js
+++ server/dataseeder.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const fastcsv = require("fast-csv");
 const { connectMySQLDB: sql } = require("./config/db");
 
-const insertCountries = async (connection) => {
+const insertCountries = (connection) => {
   let csvData = [];
   fs.createReadStream("./datasets/countries.csv")
     .pipe(fastcsv.parse())
@@ -18,9 +18,7 @@ const insertCountries = async (connection) => {
           let query = "INSERT INTO Country (countryName, isoCode) VALUES ?";
           connection.query(query, [csvData], (err, result) => {
             if (err) {
-              console.log(
-                `Error inserting data into Country table: ${err?.sqlMessage}`
-              );
+              console.error(`Error: ${err?.sqlMessage}`);
             } else {
               console.log(
                 `Successfully inserted ${result?.affectedRows} rows into Country table...`
@@ -32,11 +30,43 @@ const insertCountries = async (connection) => {
     });
 };
 
+const insertCountryVaccinations = (connection) => {
+  let csvData = [];
+  fs.createReadStream("./datasets/country_vaccinations.csv")
+    .pipe(fastcsv.parse())
+    .on("data", (data) => csvData.push(data))
+    .on("end", () => {
+      csvData.shift(); // Remove header row
+      // Check if there are any records in Table
+      connection.query(
+        "SELECT * FROM CountryVaccinated LIMIT 1",
+        (err, result) => {
+          // Insert records if table is empty
+          if (result.length === 0) {
+            // Bulk insert into table
+            let query =
+              "INSERT INTO CountryVaccinated (isoCode, totalFullyVaccinated, totalVaccinated, vaccinatedPercent) VALUES ?";
+            connection.query(query, [csvData], (err, result) => {
+              if (err) {
+                console.error(`Error: ${err?.sqlMessage}`);
+              } else {
+                console.log(
+                  `Successfully inserted ${result?.affectedRows} rows into CountryVaccinated table...`
+                );
+              }
+            });
+          }
+        }
+      );
+    });
+};
+
 const seedData = () => {
   const db = sql();
   db.getConnection((err, connection) => {
     try {
       insertCountries(connection);
+      insertCountryVaccinations(connection);
     } catch (err) {
       console.error(err);
     } finally {
