a99b754e98e11cb59dc84b570a26509ad7122181
HanYi <nghanyi1997@gmail.com>
2021-10-13 10:45:38 +0800

    #5 modularise data seeder codes

--- server/dataseeder.js
+++ server/dataseeder.js
@@ -2,68 +2,66 @@ const fs = require("fs");
 const fastcsv = require("fast-csv");
 const { connectMySQLDB: sql } = require("./config/db");
 
-const insertCountries = (connection) => {
-  let csvData = [];
-  fs.createReadStream("./datasets/countries.csv")
-    .pipe(fastcsv.parse())
-    .on("data", (data) => csvData.push(data))
-    .on("end", () => {
-      csvData.shift(); // Remove header row
+const getCSVData = (path) => {
+  return new Promise((resolve, reject) => {
+    const csvData = [];
+    fs.createReadStream(path)
+      .pipe(fastcsv.parse({ headers: true }))
+      .on("data", (data) => csvData.push(data))
+      .on("end", () => {
+        resolve(csvData);
+      });
+  });
+};
 
-      // Check if there are any records in Table
-      connection.query("SELECT * FROM Country LIMIT 1", (err, result) => {
-        // Insert records if table is empty
-        if (result.length === 0) {
-          // Bulk insert into table
-          let query = "INSERT INTO Country (countryName, isoCode) VALUES ?";
-          connection.query(query, [csvData], (err, result) => {
-            if (err) {
-              console.error(`Error: ${err?.sqlMessage}`);
-            } else {
-              console.log(
-                `Successfully inserted ${result?.affectedRows} rows into Country table...`
-              );
-            }
-          });
+const insertCountries = (connection) => {
+  // Check if there are any records in Table
+  connection.query("SELECT * FROM Country LIMIT 1", async (err, result) => {
+    // Insert records if table is empty
+    if (result.length === 0) {
+      const data = await getCSVData("./datasets/countries.csv");
+      const query = "INSERT INTO Country (countryName, isoCode) VALUES ?";
+      // Execute query to insert data into db
+      connection.query(query, [data], (err, result) => {
+        if (err) {
+          console.error(`Error: ${err?.sqlMessage}`);
+        } else {
+          console.log(
+            `Successfully inserted ${result?.affectedRows} rows into Country table...`
+          );
         }
       });
-    });
+    }
+  });
 };
 
 const insertCountryVaccinations = (connection) => {
-  let csvData = [];
-  fs.createReadStream("./datasets/country_vaccinations.csv")
-    .pipe(fastcsv.parse())
-    .on("data", (data) => csvData.push(data))
-    .on("end", () => {
-      csvData.shift(); // Remove header row
-      // Check if there are any records in Table
-      connection.query(
-        "SELECT * FROM CountryVaccinated LIMIT 1",
-        (err, result) => {
-          // Insert records if table is empty
-          if (result.length === 0) {
-            // Bulk insert into table
-            let query =
-              "INSERT INTO CountryVaccinated (isoCode, totalFullyVaccinated, totalVaccinated, vaccinatedPercent) VALUES ?";
-            connection.query(query, [csvData], (err, result) => {
-              if (err) {
-                console.error(`Error: ${err?.sqlMessage}`);
-              } else {
-                console.log(
-                  `Successfully inserted ${result?.affectedRows} rows into CountryVaccinated table...`
-                );
-              }
-            });
+  connection.query(
+    "SELECT * FROM CountryVaccinated LIMIT 1",
+    async (err, result) => {
+      // Insert records if table is empty
+      if (result.length === 0) {
+        const data = await getCSVData("./datasets/country_vaccinations.csv");
+        const query =
+          "INSERT INTO CountryVaccinated (isoCode, totalFullyVaccinated, totalVaccinated, vaccinatedPercent) VALUES ?";
+        // Execute query to insert data into db
+        connection.query(query, [data], (err, result) => {
+          if (err) {
+            console.error(`Error: ${err?.sqlMessage}`);
+          } else {
+            console.log(
+              `Successfully inserted ${result?.affectedRows} rows into CountryVaccinated table...`
+            );
           }
-        }
-      );
-    });
+        });
+      }
+    }
+  );
 };
 
 const seedData = () => {
   const db = sql();
-  db.getConnection((err, connection) => {
+  db.getConnection(async (err, connection) => {
     try {
       insertCountries(connection);
       insertCountryVaccinations(connection);
