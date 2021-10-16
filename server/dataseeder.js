fd90de0b9da5ecb6805b46dd61b380606798f1e0
HanYi <nghanyi1997@gmail.com>
2021-10-16 17:36:01 +0800

    Clean code

--- server/dataseeder.js
+++ server/dataseeder.js
@@ -77,33 +77,6 @@ const insertCountryVaccinations = () => {
 };
 
 /**
- * Insert pcr clinic data from csv into database.
- *
- * Checks if there are any data in the table before insertion.
- */
-const insertPCRClinics = () => {
-  return new Promise((resolve, reject) => {
-    sql.query(query.SELECT_PCRCLINIC_TOP1, async (err, result) => {
-      // Insert records if table is empty
-      if (result.length === 0) {
-        const data = await getCSVData("./datasets/pcr_clinics.csv");
-        sql.query(query.INSERT_PCRCLINIC, [data], (err, result) => {
-          if (err) {
-            console.error(`Error: ${err?.sqlMessage}`);
-            reject();
-          } else {
-            console.log(
-              `Successfully inserted ${result?.affectedRows} rows into pcr_clinic table...`
-            );
-            resolve();
-          }
-        });
-      }
-    });
-  });
-};
-
-/**
  * Insert travel restriction data from csv into database.
  *
  * Checks if there are any data in the table before insertion.
@@ -144,12 +117,39 @@ const insertTravelRestrictions = () => {
   });
 };
 
+/**
+ * Insert pcr clinic data from csv into database.
+ *
+ * Checks if there are any data in the table before insertion.
+ */
+const insertPCRClinics = () => {
+  return new Promise((resolve, reject) => {
+    sql.query(query.SELECT_PCRCLINIC_TOP1, async (err, result) => {
+      // Insert records if table is empty
+      if (result.length === 0) {
+        const data = await getCSVData("./datasets/pcr_clinics.csv");
+        sql.query(query.INSERT_PCRCLINIC, [data], (err, result) => {
+          if (err) {
+            console.error(`Error: ${err?.sqlMessage}`);
+            reject();
+          } else {
+            console.log(
+              `Successfully inserted ${result?.affectedRows} rows into pcr_clinic table...`
+            );
+            resolve();
+          }
+        });
+      }
+    });
+  });
+};
+
 const seedData = async () => {
   try {
     await insertCountries();
-    insertCountryVaccinations();
-    insertPCRClinics();
-    insertTravelRestrictions();
+    await insertCountryVaccinations();
+    await insertTravelRestrictions();
+    await insertPCRClinics();
   } catch (err) {
     console.error(err);
   }
