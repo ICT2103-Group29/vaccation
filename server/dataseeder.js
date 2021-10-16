26d7ab94370c852412fbdff34b43fd94b754ba6c
HanYi <nghanyi1997@gmail.com>
2021-10-16 13:58:19 +0800

    #5 document code

--- server/dataseeder.js
+++ server/dataseeder.js
@@ -3,6 +3,12 @@ const fastcsv = require("fast-csv");
 const query = require("./constants/queries/dataseeder");
 const { connectMySQLDB: sql } = require("./config/db");
 
+/**
+ * Returns data retrieved from csv.
+ *
+ * @param {string} path to csv file.
+ * @returns {Array} csv data
+ */
 const getCSVData = (path) => {
   return new Promise((resolve, reject) => {
     const csvData = [];
@@ -16,14 +22,17 @@ const getCSVData = (path) => {
   });
 };
 
+/**
+ * Insert country data from csv into database.
+ *
+ * Checks if there are any data in the table before insertion.
+ * @param {*} connection to mysql.
+ */
 const insertCountries = (connection) => {
-  // Check if there are any records in Table
   connection.query(query.SELECT_COUNTRY_TOP1, async (err, result) => {
     // Insert records if table is empty
     if (result.length === 0) {
       const data = await getCSVData("./datasets/countries.csv");
-
-      // Execute query to insert data into db
       connection.query(query.INSERT_COUNTRY, [data], (err, result) => {
         if (err) {
           console.error(`Error: ${err?.sqlMessage}`);
@@ -37,12 +46,17 @@ const insertCountries = (connection) => {
   });
 };
 
+/**
+ * Insert country vaccination data from csv into database.
+ *
+ * Checks if there are any data in the table before insertion.
+ * @param {*} connection to mysql.
+ */
 const insertCountryVaccinations = (connection) => {
   connection.query(query.SELECT_COUNTRYVACC_TOP1, async (err, result) => {
     // Insert records if table is empty
     if (result.length === 0) {
       const data = await getCSVData("./datasets/country_vaccinations.csv");
-      // Execute query to insert data into db
       connection.query(query.INSERT_COUNTRYVACC, [data], (err, result) => {
         if (err) {
           console.error(`Error: ${err?.sqlMessage}`);
@@ -56,12 +70,17 @@ const insertCountryVaccinations = (connection) => {
   });
 };
 
+/**
+ * Insert pcr clinic data from csv into database.
+ *
+ * Checks if there are any data in the table before insertion.
+ * @param {*} connection to mysql.
+ */
 const insertPCRClinics = (connection) => {
   connection.query(query.SELECT_PCRCLINIC_TOP1, async (err, result) => {
     // Insert records if table is empty
     if (result.length === 0) {
       const data = await getCSVData("./datasets/pcr_clinics.csv");
-      // Execute query to insert data into db
       connection.query(query.INSERT_PCRCLINIC, [data], (err, result) => {
         if (err) {
           console.error(`Error: ${err?.sqlMessage}`);
@@ -75,11 +94,19 @@ const insertPCRClinics = (connection) => {
   });
 };
 
+/**
+ * Insert travel restriction data from csv into database.
+ *
+ * Checks if there are any data in the table before insertion.
+ * @param {*} connection to mysql.
+ */
 const insertTravelRestrictions = (connection) => {
   connection.query(query.SELECT_COUNTRYRES_TOP1, async (err, result) => {
     // Insert records if table is empty
     if (result.length === 0) {
       const data = await getCSVData("./datasets/travel_restrictions.csv");
+
+      // Check for empty values
       const cleaned_data = [];
       data.forEach((item) => {
         if (
@@ -91,7 +118,7 @@ const insertTravelRestrictions = (connection) => {
         }
         cleaned_data.push([item[1], item[2], item[3]]);
       });
-      // Execute query to insert data into db
+
       connection.query(
         query.INSERT_COUNTRYRES,
         [cleaned_data],
