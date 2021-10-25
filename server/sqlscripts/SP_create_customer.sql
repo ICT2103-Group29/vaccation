340973aa1a02df1fbf192a3d6348468fdd180787
HanYi <nghanyi1997@gmail.com>
2021-10-25 15:39:36 +0800

    Upload sql scripts to create stored procedures

---
+++ server/sqlscripts/SP_create_customer.sql
@@ -0,0 +1,36 @@
+USE vaccationdb;
+
+DELIMITER $$
+CREATE PROCEDURE Create_Customer
+(
+	IN bookingId INT,
+    
+    IN mobileNumber INT,
+    IN firstName VARCHAR(255),
+    IN lastName VARCHAR(255),
+    IN email VARCHAR(255),
+	
+    IN passportNumber VARCHAR(20),
+    IN nationality VARCHAR(50),
+    IN placeOfIssue VARCHAR(255),
+    IN expiryDate DATE,
+    IN dob DATE
+)
+BEGIN 
+--  DECLARE EXIT HANDLER FOR SQLEXCEPTION
+--     BEGIN
+--       ROLLBACK;
+--     END;
+START TRANSACTION;
+-- passport
+INSERT INTO passport (passport_number, nationality, place_of_issue, expiry_date, dob)
+  VALUES(passportNumber, nationality, placeOfIssue, expiryDate, dob);
+-- customer
+INSERT INTO customer (mobile_number, first_name, last_name, email, passport)
+  VALUES(mobileNumber, firstName, lastName, email, passportNumber);
+SET @customer_id = LAST_INSERT_ID();
+INSERT INTO customer_booking (booking_id, user_id)
+	VALUES (bookingId, @customer_id);
+COMMIT;
+END $$
+DELIMITER ;
