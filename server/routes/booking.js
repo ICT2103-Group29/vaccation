c02ba43d1da62eb633b4129e2dcdbae297062022
HanYi <nghanyi1997@gmail.com>
2021-10-29 16:40:24 +0800

    Add swagger to booking apis

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -8,8 +8,38 @@ const express = require("express");
 const booking = require("../controllers/booking");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/bookings:
+ *  post:
+ *    summary: Create booking
+ *    tags: [Booking]
+ *    requestBody:
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: JSON booking object
+ *      '500':
+ *        description: Server error
+ */
 router.post("/", booking.create);
 
+/**
+ * @swagger
+ * /api/bookings/{bookingId}:
+ *  get:
+ *    summary: Get booking details
+ *    tags: [Booking]
+ *    parameters:
+ *      - in: path
+ *        name: bookingId
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: JSON booking object
+ *      '500':
+ *        description: Server error
+ */
 router.get("/:bookingId", booking.getBookingDetails);
 
 module.exports = router;
