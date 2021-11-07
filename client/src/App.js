91d82bb3bb48beff0eb85ba82fe19e2d712fcfd4
bellesim <bellesimbeier@gmail.com>
2021-11-07 11:26:07 +0800

    changed files to js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import searchFlights from "./components/Flight/SearchFlights";
+import SearchFlights from "./components/Flight/SearchFlights";
 import Results from "./components/Flight/Results";
 import PassengerDetails from "./components/Flight/PassengerDetails";
 import Payment from "./components/Flight/Payment";
@@ -16,11 +16,22 @@ function App() {
       <Navbar />
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/searchFlights" component={searchFlights} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/passengerDetails" component={PassengerDetails} />
-          <Route exact path="/payment" component={Payment} />
+          <Route exact key="home" path="/" component={Home} />
+
+          <Route
+            exact
+            key="searchFlights"
+            path="/searchFlights"
+            component={SearchFlights}
+          />
+          <Route exact key="results" path="/results" component={Results} />
+          <Route
+            exact
+            key="passengerDetails"
+            path="/passengerDetails"
+            component={PassengerDetails}
+          />
+          <Route exact key="payment" path="/payment" component={Payment} />
         </Switch>
       </Router>
 
