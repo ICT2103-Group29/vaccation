bd99349b913629c1788ea246acb0bfc4d7ebb7da
bellesim <bellesimbeier@gmail.com>
2021-11-08 14:51:34 +0800

    add links to cards

--- client/src/components/Flight/SearchFlights.js
+++ client/src/components/Flight/SearchFlights.js
@@ -5,22 +5,12 @@ import "../../assets/css/font.css";
 import "../../assets/css/searchFlights.css";
 import LargeCard from "../Shared/LargeCard";
 import CardGradient from "../Shared/CardGradient";
-import {
-  Button,
-  InputNumber,
-  Form,
-  Select,
-  DatePicker,
-  Checkbox,
-  Input,
-} from "antd";
+import { Button, InputNumber, Form, Select, DatePicker } from "antd";
 import "../../assets/css/button.css";
-import { getCountries, getOpenCountries } from "../../api";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
-
+import { getCountries } from "../../api";
 import moment from "moment";
 
-function SearchFlights(props) {
+function SearchFlights() {
   const [data, setData] = useState({
     //can be any variable name
     countriesInfo: {
@@ -59,12 +49,6 @@ function SearchFlights(props) {
     arrivalDate: "",
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e);
-    clear();
-  };
-
   const clear = () => {
     setPostData({
       countryFrom: "",
@@ -75,22 +59,29 @@ function SearchFlights(props) {
     });
   };
 
-  const cardTypes = [
-    { id: 1, name: "Booking" },
-    { id: 2, name: "Travel Restrictions" },
-    { id: 3, name: "Pre-Departure COVID Test" },
+  const links = [
+    {
+      to: "/booking",
+      name: "Booking",
+    },
+    {
+      to: "/travelRestrictions",
+      name: "Travel Restrictions",
+    },
+    {
+      to: "/PCRClinics",
+      name: "Pre-Departure COVID Test",
+    },
   ];
   function disabledDate(current) {
     // Can not select days before today and today
     return current && current < moment().endOf("day");
   }
 
-  const onFinish = (values) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+  const onFinish = (e) => {
+    e.preventDefault();
+    console.log("Success:", e);
+    clear();
   };
 
   return (
@@ -100,139 +91,6 @@ function SearchFlights(props) {
       </h1>
 
       <LargeCard>
-        {/* <Form
-          layout="vertical"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <div class="font-bold ">
-            <div class=" ">
-              {!data.loading && (
-                <Fragment>
-                  <Form.Item
-                    label="Where From "
-                    name="countryFrom"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please select country!",
-                      },
-                    ]}
-                  >
-                    <Select
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          countryFrom: e,
-                        })
-                      }
-                    >
-                      {data.countriesInfo.countries.map((country) => (
-                        <Select.Option
-                          name="countrySelectedFrom"
-                          value={country.iso}
-                        >
-                          {country.country_name}
-                        </Select.Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  <Form.Item
-                    label="Where To"
-                    name="countryTo"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please select country!",
-                      },
-                    ]}
-                  >
-                    <Select
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          countryTo: e,
-                        })
-                      }
-                    >
-                      {data.countriesInfo.countries.map((country) => (
-                        <Select.Option
-                          name="countrySelectedTo"
-                          value={country.iso}
-                        >
-                          {country.country_name}
-                        </Select.Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  <Form.Item label="No of Passengers" name="noOfPassengers">
-                    <InputNumber
-                      min={1}
-                      max={10}
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          noOfPassengers: e,
-                        })
-                      }
-                    ></InputNumber>
-                  </Form.Item>
-                </Fragment>
-              )}
-            </div>
-            <div class="">
-              <Form.Item
-                name="departureDate"
-                label="Departure Date"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please select departure date!",
-                  },
-                ]}
-              >
-                <DatePicker
-                  onChange={(e) =>
-                    setPostData({
-                      ...postData,
-                      departureDate: e,
-                    })
-                  }
-                  disabledDate={disabledDate}
-                />
-              </Form.Item>
-              <Form.Item
-                label="Arrival Date"
-                name="arrivalDate"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please select arrival date!",
-                  },
-                ]}
-              >
-                <DatePicker
-                  onChange={(e) =>
-                    setPostData({
-                      ...postData,
-                      arrivalDate: e,
-                    })
-                  }
-                  disabledDate={disabledDate}
-                />
-              </Form.Item>
-            </div>
-            <Form.Item>
-              <Link to="/results">
-                <Button type="primary" onClick={clear} htmlType="submit">
-                  Search Flights
-                </Button>
-              </Link>
-            </Form.Item>
-          </div>
-        </Form> */}
         <Form
           layout="vertical"
           initialValues={{ remember: true }}
@@ -301,7 +159,16 @@ function SearchFlights(props) {
                       ))}
                     </Select>
                   </Form.Item>
-                  <Form.Item label="No of Passengers" name="noOfPassengers">
+                  <Form.Item
+                    label="No of Passengers"
+                    name="noOfPassengers"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please select number of passengers!",
+                      },
+                    ]}
+                  >
                     <InputNumber
                       min={1}
                       max={10}
@@ -366,10 +233,10 @@ function SearchFlights(props) {
           </div>
         </Form>
       </LargeCard>
-      <div class="flex justify-around  mt-32 m-auto w-11/12">
-        {cardTypes.map((item) => (
+      <div class="flex  mt-32 m-auto w-11/12 ">
+        {links.map((link) => (
           <CardGradient>
-            <p>{item.name}</p>
+            <Link to={link.to}>{link.name}</Link>
           </CardGradient>
         ))}
       </div>
