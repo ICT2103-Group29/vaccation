a0ec44baa28377b62ef2bf02c7cee4bb69b2b3ea
HanYi <nghanyi1997@gmail.com>
2021-11-15 16:49:45 +0800

    Implement PCR clinic api call"

--- client/src/components/PCRclinic/PCRClinics.js
+++ client/src/components/PCRclinic/PCRClinics.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../assets/css/font.css";
 
 import LargeCard from "../Shared/LargeCard";
@@ -6,20 +6,35 @@ import { Button } from "antd";
 import "../../assets/css/button.css";
 
 import { Form, Select, DatePicker } from "antd";
+import PCRCard from "./PCRCard";
+import { getPCRClinics } from "../../api";
 
-function PCRClinics(){
-  return(
+function PCRClinics() {
+  const [clinics, setClinics] = useState([]);
+
+  const retrieveData = async () => {
+    const result = await getPCRClinics();
+    setClinics(result.data);
+  };
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  return (
     <div class>
-      <h1 class=" text-5xl text-center pt-20 text-blue-900 pb-10"><b>Looking for a clinic near you for your PCR testing?</b></h1>
+      <h1 class=" text-5xl text-center pt-20 text-blue-900 pb-10">
+        <b>Looking for a clinic near you for your PCR testing?</b>
+      </h1>
 
       <link
-    rel="stylesheet"
-    href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
-  />
-   <div class="flex justify-center items-center">
-  <div class="relative flex-wrap items-stretch mb-15  m-5 px-20 w-2/3 ">
-    <span
-      class="
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
+      />
+      <div class="flex justify-center items-center">
+        <div class="relative flex-wrap items-stretch mb-15  m-5 px-20 w-2/3 ">
+          <span
+            class="
         z-10
         h-full
         leading-snug
@@ -36,13 +51,13 @@ function PCRClinics(){
         pl-3
         py-3
       "
-    >
-      <i class="fas fa-search"></i>
-    </span>
-    <input
-      type="text"
-      placeholder="Search"
-      class="
+          >
+            <i class="fas fa-search"></i>
+          </span>
+          <input
+            type="text"
+            placeholder="Search"
+            class="
         px-3
         py-3
         placeholder-gray-400
@@ -57,79 +72,39 @@ function PCRClinics(){
         w-full
         pl-10
       "
-    />
-    </div>
-  </div>
-  <div class="relative">
-    
-</div>
+          />
+        </div>
+      </div>
+      <div class="relative"></div>
 
       <div class="p-2 flex flex-row justify-center ">
         <div class="m-5">
-          <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-52 h-14 rounded-full text-xl">Filter option 1</button>
+          <button
+            type="button"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-52 h-14 rounded-full text-xl"
+          >
+            Filter option 1
+          </button>
         </div>
         <div class="m-5 ">
-          <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-52 h-14 rounded-full text-xl">Filter option 2</button>
+          <button
+            type="button"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-52 h-14 rounded-full text-xl"
+          >
+            Filter option 2
+          </button>
         </div>
       </div>
-    
 
       <div class="flex flex-col pb-20">
         <div class="container mx-auto  my-4 px-4 py-4 bg-blue-800">
-
-          <div class="bg-white m-10 p-5 rounded-2xl"> 
-          <p class="text-left ..."><h1 class="text-2xl"><b>Fullerton Healthcare</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Location:</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Contact:</b></h1></p>
-            <div class="bg-purple-200 m-10 p-5 h-72 rounded-2xl">
-              <p class="text-left ..."><h1 class="text-2xl"><b> Monday - Thursday</b></h1></p>
-              <p class="text-left ..."><h1 class="text-2xl"><b>Opening Hours: </b></h1></p>
-            </div>
-          </div>
-          <div class="bg-white m-10 p-5 rounded-2xl"> 
-          <p class="text-left ..."><h1 class="text-2xl"><b>Fullerton Healthcare</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Location:</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Contact:</b></h1></p>
-            <div class="bg-purple-200 m-10 p-5 h-72 rounded-2xl">
-              <p class="text-left ..."><h1 class="text-2xl"><b> Monday - Thursday</b></h1></p>
-              <p class="text-left ..."><h1 class="text-2xl"><b>Opening Hours: </b></h1></p>
-            </div>
-          </div>
-          <div class="bg-white m-10 p-5 rounded-2xl"> 
-          <p class="text-left ..."><h1 class="text-2xl"><b>Fullerton Healthcare</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Location:</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Contact:</b></h1></p>
-            <div class="bg-purple-200 m-10 p-5 h-72 rounded-2xl">
-              <p class="text-left ..."><h1 class="text-2xl"><b> Monday - Thursday</b></h1></p>
-              <p class="text-left ..."><h1 class="text-2xl"><b>Opening Hours: </b></h1></p>
-            </div>
-          </div>
-          <div class="bg-white m-10 p-5 rounded-2xl"> 
-          <p class="text-left ..."><h1 class="text-2xl"><b>Fullerton Healthcare</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Location:</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Contact:</b></h1></p>
-            <div class="bg-purple-200 m-10 p-5 h-72 rounded-2xl">
-              <p class="text-left ..."><h1 class="text-2xl"><b> Monday - Thursday</b></h1></p>
-              <p class="text-left ..."><h1 class="text-2xl"><b>Opening Hours: </b></h1></p>
-            </div>
-          </div>
-          <div class="bg-white m-10 p-5 rounded-2xl"> 
-          <p class="text-left ..."><h1 class="text-2xl"><b>Fullerton Healthcare</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Location:</b></h1></p>
-          <p class="text-left ..."><h1 class="text-2xl"><b>Contact:</b></h1></p>
-            <div class="bg-purple-200 m-10 p-5 h-72 rounded-2xl">
-              <p class="text-left ..."><h1 class="text-2xl"><b> Monday - Thursday</b></h1></p>
-              <p class="text-left ..."><h1 class="text-2xl"><b>Opening Hours: </b></h1></p>
-            </div>
-          </div>
-        
-        
+          {clinics.map((clinic) => (
+            <PCRCard data={clinic} />
+          ))}
         </div>
-
       </div>
     </div>
-    
-  )
+  );
 }
 
 export default PCRClinics;
