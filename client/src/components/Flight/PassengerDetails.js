fcdd74ce451ef187999de7f57c2a24f005fbcf51
Belle Sim <bellesimbeier@gmail.com>
2021-11-22 23:58:52 +0800

    add validation checks

--- client/src/components/Flight/PassengerDetails.js
+++ client/src/components/Flight/PassengerDetails.js
@@ -46,82 +46,102 @@ const PassengerDetails = () => {
     console.log(postData);
   };
 
-  // const onFinish = (e) => {
-  //   e.preventDefault();
-
-  //   console.log("Success:", e);
-  // };
-
-  return (
-    <div>
-      <h1 class="text-4xl font-bold text-center text-blue-800 mt-20">
-        Plan Ahead and Book with Confidence
-      </h1>
-      <Fragment>
-        <div class="m-auto">
-          <h3 class="text-2xl font-bold pl-64">Passenger Detail</h3>
-          <LargeCard>
-            <Form
-              layout="vertical"
-              nitialValues={{ remember: true }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={handleSubmit}
-            >
-              <div class="font-bold">
-                <div class=" ">
-                  <Form.Item label="First Name "
-                    name="firstName"
-                    rules={[{ required: true, message: 'Please input your first name!' }]}
-
-                  >
-                    <Input
-                      size="large"
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          firstName: e.target.value,
-                        })
-                      }
-                      placeholder="Enter First Name"
-                    />
-                  </Form.Item>
-                  <Form.Item label="Last Name"
-                    name="lastName"
-                    rules={[{ required: true, message: 'Please input your last name!' }]}>
-                    <Input
-                      size="large"
-                      placeholder="Enter Last Name"
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          lastName: e.target.value,
-                        })
-                      }
-                    />
-                  </Form.Item>
-                  <Form.Item label="Email"
-                    name="email"
-                    rules={[{ required: true, type="email", message: 'Please input valid email!' }]}            
-                  >
-                    <Input
-                      size="large"
-                      placeholder="Enter Email"
-                      onChange={(e) =>
-                        setPostData({
-                          ...postData,
-                          email: e.target.value,
-                        })
-                      }
-                    />
-                  </Form.Item>
-                </div>
-                <div class="">
-                  <div class="flex">
-                    <Form.Item label="Passport Number"
-                    name="passportNo"
-                    rules={[{ required: true, message: 'Please input your passport number!' }]}>
+ 
+    return (
+      <div>
+        <h1 class="text-4xl font-bold text-center text-blue-800 mt-20">
+          Plan Ahead and Book with Confidence
+        </h1>
+        <Fragment>
+          <div class="m-auto">
+            <h3 class="text-2xl font-bold pl-64">Passenger Detail</h3>
+            <LargeCard>
+              <Form
+                layout="vertical"
+                nitialValues={{ remember: true }}
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={handleSubmit}
+              >
+                <div class="font-bold">
+                  <div class=" ">
+                    <Form.Item
+                      label="First Name "
+                      name="firstName"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your first name!",
+                        },
+                      ]}
+                    >
+                      <Input
+                        size="large"
+                        onChange={(e) =>
+                          setPostData({
+                            ...postData,
+                            firstName: e.target.value,
+                          })
+                        }
+                        placeholder="Enter First Name"
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      label="Last Name"
+                      name="lastName"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your last name!",
+                        },
+                      ]}
+                    >
+                      <Input
+                        size="large"
+                        placeholder="Enter Last Name"
+                        onChange={(e) =>
+                          setPostData({
+                            ...postData,
+                            lastName: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      label="Email"
+                      name="email"
+                      rules={[
+                        {
+                          required: true,
+                          type: "email",
+                          message: "Please input valid email!",
+                        },
+                      ]}
+                    >
+                      <Input
+                        size="large"
+                        placeholder="Enter Email"
+                        onChange={(e) =>
+                          setPostData({
+                            ...postData,
+                            email: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Item>
+                  </div>
+                  <div class="">
+                    <Form.Item
+                      label="Passport Number"
+                      name="passportNo"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your passport number!",
+                        },
+                      ]}
+                    >
                       <Input
                         size="large"
                         placeholder="Enter Passport Number"
@@ -133,10 +153,16 @@ const PassengerDetails = () => {
                         }
                       />
                     </Form.Item>
-                    <Form.Item label="Nationality"
-                    name="nationality"
-                    rules={[{ required: true, message: 'Please input your nationality!' }]}>
-                    
+                    <Form.Item
+                      label="Nationality"
+                      name="nationality"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your nationality!",
+                        },
+                      ]}
+                    >
                       <Input
                         size="large"
                         placeholder="Enter Nationality"
@@ -148,10 +174,16 @@ const PassengerDetails = () => {
                         }
                       />
                     </Form.Item>
-                    <Form.Item label="Place of Issue"
-                    name="placeOfIssue"
-                    rules={[{ required: true, message: 'Please input your place of issue' }]}>
-                    
+                    <Form.Item
+                      label="Place of Issue"
+                      name="placeOfIssue"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your place of issue",
+                        },
+                      ]}
+                    >
                       <Input
                         size="large"
                         placeholder="Enter Place of Issue"
@@ -164,8 +196,17 @@ const PassengerDetails = () => {
                       />
                     </Form.Item>
                   </div>
-                  <div class="flex ">
-                    <Form.Item label="Expiry">
+                  <div class="flex justify-evenly">
+                    <Form.Item
+                      label="Expiry"
+                      name="expiry"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your expiry date!",
+                        },
+                      ]}
+                    >
                       <DatePicker
                         onChange={(e) =>
                           setPostData({
@@ -186,9 +227,16 @@ const PassengerDetails = () => {
                         }
                       /> */}
                     </Form.Item>
-                    <Form.Item label="DOB"
-                     name="DOB"
-                    rules={[{ required: true, message: 'Please input your date of birth' }]}>      
+                    <Form.Item
+                      label="DOB"
+                      name="DOB"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your date of birth",
+                        },
+                      ]}
+                    >
                       <DatePicker
                         onChange={(e) =>
                           setPostData({
@@ -210,18 +258,21 @@ const PassengerDetails = () => {
                     </Form.Item>
                   </div>
                 </div>
-              </div>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" onClick={handleSubmit}>
-                  Search Flights
-                </Button>
-              </Form.Item>
-            </Form>
-          </LargeCard>
-        </div>
-      </Fragment>
-    </div>
-  );
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    onClick={handleSubmit}
+                  >
+                    Search Flights
+                  </Button>
+                </Form.Item>
+              </Form>
+            </LargeCard>
+          </div>
+        </Fragment>
+      </div>
+    );
 };
 
 export default PassengerDetails;
