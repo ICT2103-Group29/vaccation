234f5641ee14772411aaa9319bcccadc2f50f4d4
jy2308 <bluecandyfloss2@gmail.com>
2021-11-24 15:16:15 +0800

    updated travel restrictions page styling

--- client/src/components/Restrictions/RestrictionsCard.js
+++ client/src/components/Restrictions/RestrictionsCard.js
@@ -4,21 +4,34 @@ const RestrictionsCard = ({ data }) => {
   return (
     <div class="bg-white m-10 p-5 rounded-2xl">
       <p class="text-left ...">
-        <h1 class="text-2xl">
+        {/* <h1 class="text-2xl"> */}
+        <h1 class="text-3xl">
           <b>{data.country_name}</b>
         </h1>
       </p>
       <p class="text-left ...">
-        <h1 class="text-2xl">
-          <b>Restrictions: {data.restrictions}</b>
-        </h1>
+        {/* <h1 class="text-2xl"> */}
+        <h1 class="text-2xl text-blue-500">
+          {/* <b>Restrictions: {data.restrictions}</b> */}
+          <b>Restrictions: </b>
+          </h1>
+          <p class="text-xl text-black-500">
+          {data.restrictions}
+          </p>
+        {/* </h1> */}
       </p>
       <p class="text-left ...">
-        <h1 class="text-2xl">
-          <b>Procedures: {data.procedures}</b>
-        </h1>
+        {/* <h1 class="text-2xl"> */}
+        <h1 class="text-2xl text-blue-500">
+          {/* <b>Procedures: {data.procedures}</b> */}
+          <b>Procedures: </b>
+          </h1>
+          <p class="text-xl text-black-500">
+          {data.procedures}
+          </p>
+        {/* </h1> */}
       </p>
-      <div class="bg-purple-200 m-10 p-5 rounded-2xl">
+      {/* <div class="bg-purple-200 m-10 p-5 rounded-2xl"> */}
         <h1 class="text-xl text-left">
           {/* <b
             dangerouslySetInnerHTML={{
@@ -30,7 +43,7 @@ const RestrictionsCard = ({ data }) => {
             }} */}
           {/* /> */}
         </h1>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
